6b7a2e8a7c0ee01ebb126b098a6486fdf1534706
chitranshjawre <chitranshjawre2001@gmail.com>
2025-10-01 21:54:59 +0530

    update

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,5 +1,21 @@
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./serviceAccountKey.json.json"); // adjust path
+
+// // ✅ Only initialize if not already initialized
+// const app = admin.apps.length
+//   ? admin.app()
+//   : admin.initializeApp({
+//       credential: admin.credential.cert(serviceAccount),
+//     });
+
+// module.exports = app;
+
+
+
+
+
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json.json"); // adjust path
+const serviceAccount = require("/etc/secrets/serviceAccountKey.json.json"); // adjust path
 
 // ✅ Only initialize if not already initialized
 const app = admin.apps.length
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,58 @@
+// // middleware/auth.js
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/user");
+
+// const auth = async (req, res, next) => {
+//   console.log("Auth middleware invoked");
+//   console.log("Headers received:", req.headers);
+//   console.log("Authorization header:", req.headers.authorization);
+
+//   try {
+//     const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
+//     if (!token) return res.status(401).json({ message: "No token provided" });
+
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     const user = await User.findById(decoded.id);
+//     if (!user) return res.status(401).json({ message: "Invalid token" });
+
+//     req.user = user; // attach user to request
+//     next();
+//   } catch (err) {
+//     res.status(401).json({ message: "Unauthorized", error: err.message });
+//     console.error("Auth error:", err);
+//   }
+// };
+// // middleware/auth.js
+// const admin = require("firebase-admin");
+// admin.initializeApp({
+//   credential: admin.credential.cert(require("../serviceAccountKey.json"))
+// });
+
+// module.exports = async (req, res, next) => {
+//   const token = req.headers.authorization?.split(" ")[1];
+//   if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+//   try {
+//     const decoded = await admin.auth().verifyIdToken(token);
+//     req.user = { email: decoded.email, uid: decoded.uid };
+//     next();
+//   } catch (err) {
+//     res.status(401).json({ message: "Invalid token" });
+//   }
+// };
+
+
+// module.exports = auth;
+
+
+
+
+
+
+
+
+
+
 // middleware/auth.js
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
@@ -25,7 +80,7 @@ const auth = async (req, res, next) => {
 // middleware/auth.js
 const admin = require("firebase-admin");
 admin.initializeApp({
-  credential: admin.credential.cert(require("../serviceAccountKey.json"))
+  credential: admin.credential.cert(require("/etc/secrets/serviceAccountKey.json"))
 });
 
 module.exports = async (req, res, next) => {
